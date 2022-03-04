6416995fc75e28de60981c6c422a1d9e34739a76
MineBill <minebill@protonmail.com>
2022-03-04 20:42:44 +0200

    Updated build.zig to use zigmod deps

--- build.zig
+++ build.zig
@@ -1,17 +1,29 @@
 const std = @import("std");
+const deps = @import("deps.zig");
 
 pub fn build(b: *std.build.Builder) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addStaticLibrary("readline", "src/lib.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    const exe = b.addExecutable("zig-readline", "src/main.zig");
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    deps.addAllTo(exe);
+    exe.install();
 
-    const main_tests = b.addTest("src/lib.zig");
-    main_tests.setBuildMode(mode);
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
 
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 }
