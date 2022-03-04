cc9a4887f7cb97ba90cd55ca0742ac31def01321
MineBill <minebill@protonmail.com>
2022-03-04 20:39:31 +0200

    init

---
+++ deps.zig
@@ -0,0 +1,75 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const Pkg = std.build.Pkg;
+const string = []const u8;
+
+pub const cache = ".zigmod/deps";
+
+pub fn addAllTo(exe: *std.build.LibExeObjStep) void {
+    checkMinZig(builtin.zig_version, exe);
+    @setEvalBranchQuota(1_000_000);
+    for (packages) |pkg| {
+        exe.addPackage(pkg.pkg.?);
+    }
+    var llc = false;
+    var vcpkg = false;
+    inline for (comptime std.meta.declarations(package_data)) |decl| {
+        const pkg = @as(Package, @field(package_data, decl.name));
+        inline for (pkg.system_libs) |item| {
+            exe.linkSystemLibrary(item);
+            llc = true;
+        }
+        inline for (pkg.c_include_dirs) |item| {
+            exe.addIncludeDir(@field(dirs, decl.name) ++ "/" ++ item);
+            llc = true;
+        }
+        inline for (pkg.c_source_files) |item| {
+            exe.addCSourceFile(@field(dirs, decl.name) ++ "/" ++ item, pkg.c_source_flags);
+            llc = true;
+        }
+        vcpkg = vcpkg or pkg.vcpkg;
+    }
+    if (llc) exe.linkLibC();
+    if (builtin.os.tag == .windows and vcpkg) exe.addVcpkgPaths(.static) catch |err| @panic(@errorName(err));
+}
+
+pub const Package = struct {
+    directory: string,
+    pkg: ?Pkg = null,
+    c_include_dirs: []const string = &.{},
+    c_source_files: []const string = &.{},
+    c_source_flags: []const string = &.{},
+    system_libs: []const string = &.{},
+    vcpkg: bool = false,
+};
+
+fn checkMinZig(current: std.SemanticVersion, exe: *std.build.LibExeObjStep) void {
+    const min = std.SemanticVersion.parse("null") catch return;
+    if (current.order(min).compare(.lt)) @panic(exe.builder.fmt("Your Zig version v{} does not meet the minimum build requirement of v{}", .{current, min}));
+}
+
+pub const dirs = struct {
+    pub const _root = "";
+    pub const _b0ov9fnwyq9x = cache ++ "/../..";
+};
+
+pub const package_data = struct {
+    pub const _b0ov9fnwyq9x = Package{
+        .directory = dirs._b0ov9fnwyq9x,
+        .pkg = Pkg{ .name = "readline", .path = .{ .path = dirs._b0ov9fnwyq9x ++ "/src/lib.zig" }, .dependencies = null },
+    };
+    pub const _root = Package{
+        .directory = dirs._root,
+    };
+};
+
+pub const packages = &[_]Package{
+    package_data._b0ov9fnwyq9x,
+};
+
+pub const pkgs = struct {
+    pub const readline = package_data._b0ov9fnwyq9x;
+};
+
+pub const imports = struct {
+};
---
+++ zig.mod
@@ -0,0 +1,6 @@
+id: b0ov9fnwyq9x6xw3yhd6mb94noz7ut93sa04rg6fej88d4j1
+name: readline
+main: src/lib.zig
+license: GPL-3
+description: A simple readline package
+dependencies:
