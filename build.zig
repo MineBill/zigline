b2e8a07df9b0bf864c1f019ba5e2397bbc9a09b5
MineBill <minebill@protonmail.com>
2022-03-06 02:23:39 +0200

    Remove comments from build.zig and rename exe -> example

--- build.zig
+++ build.zig
@@ -2,23 +2,16 @@ const std = @import("std");
 const deps = @import("deps.zig");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("zig-readline", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    deps.addAllTo(exe);
-    exe.install();
+    const example = b.addExecutable("zigline-example", "src/main.zig");
+    example.setTarget(target);
+    example.setBuildMode(mode);
+    deps.addAllTo(example);
+    example.install();
 
-    const run_cmd = exe.run();
+    const run_cmd = example.run();
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
